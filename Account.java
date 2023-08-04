54aa73b93e86292818f9ad9fc36625d88f6bd510
Vaishnav Akash <137373380+AkashVaishnav@users.noreply.github.com>
2023-08-04 15:07:43 +0530

    added java files

---
+++ Account.java
@@ -0,0 +1,111 @@
+package start;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Account{
+
+    private int customerNumber;
+    private int pinNumber;
+    private double checkingBalance = 0;
+    private double savingBalance = 0;
+
+    Scanner input = new Scanner(System.in);
+    DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+
+    public int setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+        return customerNumber;
+    }
+
+    public int getCustomerNumber(){
+        return customerNumber;
+    }
+
+    public int setPinNumber(int pinNumber) {
+        this.pinNumber = pinNumber;
+        return pinNumber;
+    }
+
+    public int getPinNumber(){
+        return pinNumber;
+    }
+
+    public double getCheckingBalance() {
+        return checkingBalance;
+    }
+
+    public double getSavingBalance() {
+        return savingBalance;
+    }
+
+    public double calcCheckingWithdraw(double amount) {
+        checkingBalance = (checkingBalance - amount);
+        return checkingBalance;
+    }
+    public double calcSavingWithdraw(double amount) {
+        savingBalance = (savingBalance - amount);
+        return savingBalance;
+    }
+
+    public double calcCheckingDeposit(double amount) {
+        checkingBalance = (checkingBalance + amount );
+        return checkingBalance;
+    }
+    public double calcSavingDeposit(double amount) {
+        savingBalance = (savingBalance + amount );
+        return savingBalance;
+    }
+
+    public void getCheckingWithdrawInput(){
+        System.out.println("Checking Account Balance: " +moneyFormat.format(checkingBalance));
+        System.out.print("Amount you want to withdraw from Checking Account : ");
+        double amount = input.nextDouble();
+
+        if((checkingBalance - amount) >= 0){
+            calcCheckingWithdraw(amount);
+            System.out.println("New Checking Account Balance: "+moneyFormat.format(checkingBalance));
+        }else{
+            System.out.println("Balance Cannot be Negative."+"\n");
+        }
+    }
+    
+    public void getSavingWithdrawInput(){ //
+        System.out.println("saving Account Balance: " +moneyFormat.format(savingBalance));
+        System.out.print("Amount you want to withdraw from saving Account : ");
+        double amount = input.nextDouble();
+
+        if((savingBalance - amount) >= 0){
+            calcSavingWithdraw(amount);
+            System.out.println("New Checking Account Balance: "+moneyFormat.format(savingBalance));
+        }else{
+            System.out.println("Balance Cannot be Negative."+"\n");
+        }
+    }
+
+    public void getCheckingDepositInput(){ //
+        System.out.println("Checking Account Balance: " +moneyFormat.format(checkingBalance));
+        System.out.print("Amount you want to Deposit from Checking Account : ");
+        double amount = input.nextDouble();
+
+        if((checkingBalance + amount) >= 0){
+            calcCheckingDeposit(amount);
+            System.out.println("New Checking Account Balance: "+moneyFormat.format(checkingBalance));
+        }else{
+            System.out.println("Balance Cannot be Negative."+"\n");
+        }
+    }
+
+    public void getSavingDepositInput(){ //
+        System.out.println("saving Account Balance: " +moneyFormat.format(savingBalance));
+        System.out.print("Amount you want to deposit from saving Account : ");
+        double amount = input.nextDouble();
+
+        if((savingBalance + amount) >= 0){
+            calcSavingDeposit(amount);
+            System.out.println("New saving Account Balance: "+moneyFormat.format(savingBalance));
+        }else{
+            System.out.println("Balance Cannot be Negative."+"\n");
+        }
+    }
+}
---
+++ Atm.java
@@ -0,0 +1,11 @@
+package start;
+
+import java.io.IOException;
+
+public class Atm extends OptionMenu{
+    public static void main(String[] args) throws IOException {
+        OptionMenu optionmenu = new OptionMenu();
+
+        optionmenu.getLogin();
+    }
+}
